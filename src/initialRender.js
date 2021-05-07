c9780144428d527d450da402a7bfe51e1bcf9bef
Flowertin Madalin <60981000+zantexee@users.noreply.github.com>
2021-05-08 00:02:04 +0300

    finished layout

--- src/initialRender.js
+++ src/initialRender.js
@@ -4,6 +4,21 @@ const initialComponent = () => {
   const loremText =
     '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi libero, esseofficia labore quia at repudiandae animi illo odio minus mollitia pariaturblanditiis alias recusandae impedit, neque necessitatibus quidem aspernatur!';
 
+  const navbar = document.createElement('nav');
+  navbar.classList = 'navbar flex';
+  navbar.id = '#navbar';
+
+  // Creates logo and appends it to the logo container and then the navbar
+  const logoDiv = document.createElement('div');
+  logoDiv.classList = 'logo';
+
+  const logo = document.createElement('img');
+  logo.src = './assets/imgs/logo.webp';
+  logo.alt = 'cheemsburger logo';
+  logo.classList = 'img-fit circle-border';
+
+  logoDiv.appendChild(logo);
+
   const headline = document.createElement('h1');
   const headlineText = document.createTextNode('Le Burger');
   headline.appendChild(headlineText);
@@ -12,13 +27,10 @@ const initialComponent = () => {
   const paragraphText = document.createTextNode(loremText);
   paragraph.appendChild(paragraphText);
 
-  const heroImage = document.createElement('img');
-  heroImage.src = './assets/imgs/hero.webp';
-  heroImage.alt = 'cheemsburger logo';
-
-  componentContainer.appendChild(headline);
+  navbar.appendChild(headline);
+  navbar.appendChild(logoDiv);
+  componentContainer.appendChild(navbar);
   componentContainer.appendChild(paragraph);
-  componentContainer.appendChild(heroImage);
 };
 
 export default initialComponent;
